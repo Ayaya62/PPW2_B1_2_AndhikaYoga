f4a2d572fe888375be57f8711b4c44c98f18f3ce
Ayaya62 <andhikayogapratama@mail.ugm.ac.id>
2023-09-28 14:30:45 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 //route resource
 Route::resource('/', \App\Http\Controllers\PostController::class);
+Route::get('show', [PostController::class, 'index']);
